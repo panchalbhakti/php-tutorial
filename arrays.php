de18c01ffbc9cb83f511763f2fd75d4a2ebc9e18
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-11 17:58:18 +0530

    Removing or adding in array

--- arrays.php
+++ arrays.php
@@ -106,4 +106,53 @@ var_dump($fruits);
 echo "<br>";
 echo "<br>";
 
+//Removing array items
+$cars = array("Volvo", "BMW", "Toyota");
+array_splice($cars, 1, 1);
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+unset($cars[1]);
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+array_splice($cars, 1, 2);
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+unset($cars[0], $cars[1]);
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+unset($cars["model"]);
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+$newarray = array_diff($cars, ["Mustang", 1964]);
+var_dump($newarray);
+echo "<br>";
+echo "<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+array_pop($cars);
+var_dump($newarray);
+echo "<br>";
+echo "<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+array_shift($cars);
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
 ?>
