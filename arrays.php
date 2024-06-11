ca71e5239decca08ac173bcced42857da1f35aac
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-11 17:44:07 +0530

    Array Concepts

--- arrays.php
+++ arrays.php
@@ -28,4 +28,36 @@ echo "<br>";
 array_push($cars, "Ford");
 var_dump($cars);
 
+echo "<br>";
+echo "<br>";
+
+//associative Array
+$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+var_dump($car);
+echo "<br>";
+echo "<br>";
+
+$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+echo $car["model"];
+echo "<br>";
+echo "<br>";
+
+
+$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+$car["year"] = 2024;
+var_dump($car);
+echo "<br>";
+echo "<br>";
+
+//Array declaration in multiple lines
+$cars = [
+    "Volvo",
+    "BMW",
+    "Toyota"
+  ];
+  var_dump($car);
+  echo "<br>";
+echo "<br>";
+  
+
 ?>
