04e6c583eed6a28b52c215f0b973dbfca5eb059c
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-11 17:52:15 +0530

    Operation on Arrays

--- arrays.php
+++ arrays.php
@@ -58,6 +58,52 @@ $cars = [
   var_dump($car);
   echo "<br>";
 echo "<br>";
-  
+
+$myCar = [
+    "brand" => "Ford",
+    "model" => "Mustang",
+    "year" => 1964
+  ];
+  var_dump($myCar);
+  echo "<br>";
+echo "<br>";
+
+//Accessing array element
+$cars = array("Volvo", "BMW", "Toyota");
+echo $cars[2];
+echo "<br>";
+echo "<br>";
+
+//updating array values
+$cars = array("Volvo", "BMW", "Toyota");
+$cars[1] = "Ford";
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+$cars["year"] = 2024;
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+// adding something in arrays
+$fruits = array("Apple", "Banana", "Cherry");
+$fruits[] = "Orange";
+var_dump($fruits);
+echo "<br>";
+echo "<br>";
+
+$cars = array("brand" => "Ford", "model" => "Mustang");
+$cars["color"] = "Red";
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+
+$fruits = array("Apple", "Banana", "Cherry");
+array_push($fruits, "Orange", "Kiwi", "Lemon");
+var_dump($fruits);
+echo "<br>";
+echo "<br>";
 
 ?>
