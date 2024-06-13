241aff445ce5ad3b7fe03db4b89277999e8d79e7
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-13 11:42:08 +0530

    Form in php

---
+++ form_handling.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+<form action="welcome.php" method="POST">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+<!-- Name: <input type="text" name="name">
+E-mail: <input type="text" name="email">
+Website: <input type="text" name="website">
+Comment: <textarea name="comment" rows="5" cols="40"></textarea> -->
+
+<!-- Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<input type="radio" name="gender" value="other">Other -->
+
+<!-- The HTML code of the form looks like this: -->
+<!-- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> -->
+
+</form>
+</body>
+</html>
