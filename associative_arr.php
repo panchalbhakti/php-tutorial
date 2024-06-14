311eafea738e13e882ee5c9b88123d22ef9b2035
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-14 14:50:09 +0530

    array

--- associative_arr.php
+++ associative_arr.php
@@ -10,14 +10,39 @@ echo $arr[1];
 echo "<br>";
 echo $arr[2];
 echo "<br>";
+echo "<br>";
 
 //Associative array
 $favCol = array (
-    'bhakti' => 'white',
-    'prachi' => 'black'
+    'Bhakti' => 'White',
+    'Prachi' => 'Black'
 );
-echo $favCol['bhakti'];
+echo $favCol['Bhakti'];
 echo "<br>";
-echo $favCol['prachi'];
+echo $favCol['Prachi'];
+echo "<br>";
+echo "<br>";
+
+foreach($favCol as $key => $value){
+    echo "Favourite color of $key is $value.";
+    echo "<br>";
+}
+echo "<br>";
+
+$favCar = array (
+    'Bhakti' => 'Porche',
+    'Prachi' => 'Vintage'
+);
+echo $favCar['Bhakti'];
+echo "<br>";
+echo $favCar['Prachi'];
+echo "<br>";
+echo "<br>";
+
+foreach($favCar as $key => $value){
+    echo "Favourite Car of $key is $value.";
+    echo "<br>";
+}
+
 
 ?>
