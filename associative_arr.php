8edad13c19c8235b954d30624f6bc5b3c19dc758
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-14 14:42:15 +0530

    Associative array

---
+++ associative_arr.php
@@ -0,0 +1,23 @@
+<?php
+
+//array
+$arr = array('this', 'that', 'what');
+echo $arr;
+echo "<br>";
+echo $arr[0];
+echo "<br>";
+echo $arr[1];
+echo "<br>";
+echo $arr[2];
+echo "<br>";
+
+//Associative array
+$favCol = array (
+    'bhakti' => 'white',
+    'prachi' => 'black'
+);
+echo $favCol['bhakti'];
+echo "<br>";
+echo $favCol['prachi'];
+
+?>
