9e5ea5c1196bfaf4b5e173cb278eec015208abdc
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-14 14:57:44 +0530

    Multidimentional array

--- associative_arr.php
+++ associative_arr.php
@@ -44,5 +44,18 @@ foreach($favCar as $key => $value){
     echo "<br>";
 }
 
+//Multidimentional array
+$cars = array (
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Audi",5,2),
+    array("Porshe",17,15),
+  );
+  echo "<br>";
+echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+
 
 ?>
