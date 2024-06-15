68a50f3951856d0ec73b0e6badff82a1340cb844
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-15 10:47:32 +0530

    Date&time concepts

--- dates.php
+++ dates.php
@@ -12,5 +12,38 @@
     $d = date("dS l, F y");
     echo "Today's date is $d <br>"
     ?>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+echo "<br>";
+echo "<br>";
+?>
+
+<?php
+echo "The time is " . date("h:i:sa");
+echo "<br>";
+echo "<br>";
+?>
+
+<?php
+date_default_timezone_set("America/New_York");
+echo "The time is " . date("h:i:sa");
+echo "<br>";
+echo "<br>";
+?>
+
+<?php
+//To create customize date
+//mktime(hour, minute, second, month, day, year)
+$d=mktime(11, 14, 54, 8, 12, 2014);
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
+echo "<br>";
+echo "<br>";
+?>
+
+
 </body>
 </html>
