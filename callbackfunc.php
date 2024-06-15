48d91ab757758fbca303f327090d368b0d7e6407
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-15 11:15:20 +0530

    php & json

---
+++ callbackfunc.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+function my_callback($item) {
+  return strlen($item);
+}
+
+$strings = ["apple", "orange", "banana", "coconut"];
+$lengths = array_map("my_callback", $strings);
+print_r($lengths);
+echo "<br>";
+echo "<br>";
+?>
+
+<?php
+$strings = ["apple", "orange", "banana", "coconut"];
+$lengths = array_map( function($item) { return strlen($item); } , $strings);
+print_r($lengths);
+echo "<br>";
+echo "<br>";
+?>
+
+<?php
+function exclaim($str) {
+  return $str . "! ";
+}
+
+function ask($str) {
+  return $str . "? ";
+}
+
+function printFormatted($str, $format) {
+  // Calling the $format callback function
+  echo $format($str);
+}
+
+// Pass "exclaim" and "ask" as callback functions to printFormatted()
+printFormatted("Hello world", "exclaim");
+printFormatted("Hello world", "ask");
+echo "<br>";
+echo "<br>";
+?>
+
+</body>
+</html>
---
+++ php_json.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//The json_encode() function is used to encode a value to JSON format.
+$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+echo json_encode($age);
+echo"<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+echo json_encode($cars);
+echo"<br>";
+
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+var_dump(json_decode($jsonobj));
+echo"<br>";
+
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+var_dump(json_decode($jsonobj, true));
+echo "<br>";
+
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+$obj = json_decode($jsonobj);
+
+echo $obj->Peter;
+echo " ";
+echo $obj->Ben;
+echo " ";
+echo $obj->Joe;
+echo "<br>";
+
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+$arr = json_decode($jsonobj, true);
+
+echo $arr["Peter"];
+echo " ";
+echo $arr["Ben"];
+echo " ";
+echo $arr["Joe"];
+echo "<br>";
+
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+$obj = json_decode($jsonobj);
+
+foreach($obj as $key => $value) {
+  echo $key . " => " . $value . "<br>";
+}
+
+
+?>
+</body>
+</html>
