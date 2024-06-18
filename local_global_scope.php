d16a9e709c09e78aaa0babf68eaab0f4a4ca825e
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-18 22:09:19 +0530

    Get-Post method

--- local_global_scope.php
+++ local_global_scope.php
@@ -1,15 +1,21 @@
 <?php
 
 echo "Welcome to the local, global and scope variables in php <br>";
-$a = 98; //Global Variable, 
+$a = 98; //Global Variable
+$b = 25; //Global Variable
 
 function printVal(){
     $a = 12; //Local Variable, Scope within the function
+    global $a, $b; //Gives access to the global variable
     echo $a;
+    echo "<br>";
+    echo $b;
 }
 
 echo $a;
 echo "<br>";
 printVal();
+echo "<br>";
+echo var_dump($GLOBALS);
 
 ?>
